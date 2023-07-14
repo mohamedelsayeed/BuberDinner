29939643ffe7207998227fab6588f8068c9b8046
mohamedhussein <m7mdelsayeed@hotmail.com>
2023-07-14 20:18:30 +0300

    added ErrorHandlingFilterAttribute

---
+++ BuberDinner.Api/BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace BuberDinner.Api.Filters;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+        var problemDetails = new ProblemDetails()
+        {
+            Type = "",
+            Title = "an error occured",
+            Status = (int)HttpStatusCode.InternalServerError
+        };
+        context.Result = new ObjectResult(problemDetails);
+    
+
+        context.ExceptionHandled = true;
+
+    }
+}
